5d51f629e09b5f2180460652380fcf5fbe1eba0a
Pavel Vondruška <pvon@essencebs.com>
2022-03-24 23:40:53 +0100

    Beta

--- math.js
+++ math.js
@@ -121,7 +121,7 @@ function nactiUkoly(xmlText, inElementId) {
           
           // příklad Odpověď: 12 malířů vymaluje tuto halu za %1 hodin.
           // pěkná prasárnička :) vnořená změna... ((text.replace %1).replace %2) ...
-          txt += x[i].getElementsByTagName("ukol_odpoved_zaka")[0].innerHTML.replace("%1","<input type=text id=\"task_answer"+ x[i].getAttribute("id") + "\" size=1 title=\"Zadejte výsledek\"/>");   
+          txt += x[i].getElementsByTagName("ukol_odpoved_zaka")[0].innerHTML.replace("%1","<input type=text id=\"task_answer"+ x[i].getAttribute("id") + "\" size=8 title=\"Zadejte výsledek\"/>");   
           
           txt += "<button class=\"task_check_btn\" onclick=\"tryToGoToNextStage(this.parentNode.getAttribute('task_answer1'),document.getElementById('task_answer"+ x[i].getAttribute("id") + "').value,this.parentNode.getAttribute('next_task_id'));\">Zkontroluj</button>";
           // txt += "<div class=\"task_solution\" style=\"display: none;\">";
